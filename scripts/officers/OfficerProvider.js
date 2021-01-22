1c0f5b37a695c7c1776c0862831efd87a7209615
Jisie DM <jisie.david@gmail.com>
2021-01-22 14:05:14 -0800

    Get data from glassdale api and render list of officers to the DOM

--- scripts/officers/OfficerProvider.js
+++ scripts/officers/OfficerProvider.js
@@ -1,25 +1,4 @@
-let officers = [
-  {
-    "id": 1,
-    "name": "Marques Balistreri"
-  },
-  {
-    "id": 2,
-    "name": "Alessia Bailey"
-  },
-  {
-    "id": 3,
-    "name": "Lazaro Leuschke",
-  },
-  {
-    "id": 4,
-    "name": "Braden Stoltenberg"
-  },
-  {
-    "id": 5,
-    "name": "Betty Flatley"
-  },
-]
+let officers = []
 
 export const useOfficers = () => {
   return officers.slice()
@@ -30,4 +9,11 @@ export const getOfficers = () => {
       Load database state into application state with a fetch().
       Make sure the last then() updates the officers array
   */
+  return fetch("https://criminals.glassdale.us/officers")
+    .then(response => response.json())
+    .then(parsedResponse => {
+      console.table(parsedResponse)
+      officers = parsedResponse
+    })
+
 }
