70f10640c2310b70954e438b2b48f993a2600862
Jisie DM <jisie.david@gmail.com>
2021-01-26 08:12:27 -0800

    Dropdown of convictions now contains data from API

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -23,10 +23,32 @@ const render = convictionsCollection => {
       the convictionsCollection to generate the option elements.
       Look back at the example provided above.
   */
- // TO DO: FIGURE OUT WHAT THIS IS {something.map()}
+  // TO DO: FIGURE OUT WHAT THIS IS {something.map()}
+  //  The convictionsCollection.map() will iterate through an array that looks like this:
+  // [
+  //   {
+  //     name: "arson",
+  //     id: 1
+  //   }, {
+  //     name: "murder",
+  //     id: 2
+  //   },
+  //   ...
+  // ]
+
+  // The new array that .map() gives me will look like this:
+
+  // [
+  //   "<option value="1">arson</option>",
+  //   "<option value="2">murder</option>",
+  //   ...
+  // ]
+  //  debugger
   contentTarget.innerHTML = `
         <select class="dropdown" id="crimeSelect">
             <option value="0">Please select a crime...</option>
+            ${convictionsCollection.map(conviction => `<option value="${conviction.id}">${conviction.name}</option>`).join("")
+    }
         </select>
     `
 }
