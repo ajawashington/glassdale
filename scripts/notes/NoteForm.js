6d0616316d87439b55ea0696a88901c17228ce29
Jisie DM <jisie.david@gmail.com>
2021-01-29 08:18:42 -0800

    Moving preventDefault so the date field works

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -28,8 +28,8 @@ export const NoteForm = () => {
 
 // Handle browser-generated click event in component
 eventHub.addEventListener("click", clickEvent => {
-  clickEvent.preventDefault()
   if (clickEvent.target.id === "saveNote") {
+    clickEvent.preventDefault()
     const suspect = document.getElementById("note-suspect").value
     const author = document.getElementById("note-author").value
     const date = document.getElementById("note-date").value
