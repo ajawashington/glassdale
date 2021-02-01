f5037e300451f1323bc5c0d2c17f61d2fdad2b99
BryanNilsen <bryannilsen@gmail.com>
2021-02-01 09:16:32 -0600

    updated todo

--- ALIBI-TODO.js
+++ ALIBI-TODO.js
@@ -2,26 +2,27 @@
 // When Button is clicked, display the known associates and their alibis for that specific criminal somewhere on DOM
 
 // TODO: PART 1 - Make Button - Dispatch Custom Event
-// Create an AssociateAlibi Button component that returns some button HTML representation
+// Create a ShowAssociates Button component that returns some button HTML representation
+// Add ShowAssociates button to Criminal Card - (needs import)
 // Use the Criminal’s ID in the ID property of the button - (component will most likely take in a criminal object or ID as an argument)
 // Need event listener to hear the click - so you need a reference to the Event Hub
 // AddEventListener to Event Hub that listens for that specific button click
 // Check event for id to match part of the string - (associates--1 or something like that)
-// Dispatch Custom Event named something like “Associate Clicked” and pass Id of Criminal in the detail object
-// Add ShowAssociates button to Criminal Card - (with import)
+// Dispatch Custom Event named something like "showAssociates" and pass Id of Criminal in the detail object
 
 // TODO: PART 2 - Listen for Custom Event and Display Associates on DOM
-// Which Component Needs to Listen for the AssociateClicked event?
-// ** New ModalComponent
-// Needs a place to go in the DOM - add Modal Container to index.html
+// Which Component Needs to Listen for the "showAssociates" event?
+// ** New AssociatesModalComponent
 // Create ModalComponent that takes in a Criminal object and returns some HTML representation of a criminal’s associates
-// Add EventListener to Event Hub that listens for “Asociate Clicked” event
+// Add EventListener to Event Hub that listens for "showAssociates" event
+// TEST - need event listener on DOM >> Where? Button Component
+// Needs a place to go in the DOM - add Modal Container to index.html
 // Get the Criminal ID from the detail
-// Find the criminal from the array of criminals (useCriminals)
+// Find the criminal from the array of criminals (>> import useCriminals)
 // Call the ModalComponent function and pass in the criminal as an argument and store HTML string in a variable
 // Add the resulting HTML to the DOM
 // ADD MODAL CSS
-// Import Modal.css
+// Import modal.css to main.css
 
 
 // TODO: PART 3 - REFACTOR HTML/CSS
